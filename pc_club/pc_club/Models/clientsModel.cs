f73e1e389876c28bd548dafa5e7d4c765ebebff5
nemajor1 <kolaivanov040@gmail.com>
2024-06-12 21:29:46 +0300

    add clientsModel.cs, add for this enums

---
+++ pc_club/pc_club/Models/clientsModel.cs
@@ -0,0 +1,22 @@
+﻿public class clientsModel
+{
+    public int client_id { get; set; }
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public int balance { get; set; }
+    public string username { get; set; }
+    public string password { get; set; }
+    public string status { get; set; }
+    public string role { get; set; }
+    public clientsModel(int client_id, string first_name, string last_name, int balance, string username, string password, userStatus status, userRole role)
+    {
+        this.client_id = client_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.balance = balance;
+        this.username = username;
+        this.password = password;
+        this.status = status.ToString();
+        this.role = role.ToString();
+    }
+}
