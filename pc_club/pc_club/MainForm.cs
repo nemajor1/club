b27c17776465c559196ddf9008c42a85d185f2b9
nemajor1 <kolaivanov040@gmail.com>
2024-06-13 05:59:58 +0300

    add array

--- pc_club/pc_club/MainForm.cs
+++ pc_club/pc_club/MainForm.cs
@@ -4,15 +4,16 @@ namespace pc_club
 {
     public partial class MainForm : Form
     {
-        ClientsForm clients = new ClientsForm();
+        static ClientsForm clients = new ClientsForm();
+
+        Label[] labels = { clients.label2, clients.label3, clients.label4, clients.label5, clients.label6, clients.label7, clients.label8 };
         public MainForm()
         {
             InitializeComponent();
         }
         private void button2_Click(object sender, System.EventArgs e)
         {
-            DBController.LoadClientData(clients.label2, clients.label3,
-                clients.label4, clients.label5, clients.label6, clients.label7, clients.label8);
+            DBController.LoadClientData(labels);
             ActivatedForm.ShowFormInPanel(clients, panel1);
         }
     }
--- pc_club/pc_club/Utilities/DBController.cs
+++ pc_club/pc_club/Utilities/DBController.cs
@@ -70,33 +70,34 @@ public static class DBController
 
         var parametrs = new[]
         {
-            new NpgsqlParameter("@username", username),
-            new NpgsqlParameter("@password", password)
-        };
+        new NpgsqlParameter("@username", username),
+        new NpgsqlParameter("@password", password)
+    };
+
         try
         {
-            DataTable result = ExecuteQuery(msg, parametrs);
-            return result.Rows.Count > 0;
+            // Выполняем запрос и получаем одно целое число
+            int count = Convert.ToInt32(ExecuteQuery(msg, parametrs).Rows[0][0]);
+
+            // Если count больше 0, значит, пользователь с такими данными существует
+            return count > 0;
         }
         catch (Exception e)
         {
-            MessageBox.Show($"Error during login check: {e.Message}");
+            MessageBox.Show("Неверное имя или пароль");
             return false;
         }
     }
-    public static void LoadClientData(Label _id, Label _firstname, Label _lastname, Label _balance, Label _username, Label _status, Label _role)
+    public static void LoadClientData(Label[] labels )
     {
         string msg = "SELECT client_id, first_name, last_name, balance, username, status, role FROM clients";
 
         try
         {
-            _id.Text = "";
-            _firstname.Text = "";
-            _lastname.Text = "";
-            _balance.Text = "";
-            _username.Text = "";
-            _status.Text = "";
-            _role.Text = "";
+            foreach (var label in labels)
+            {
+                label.Text = "";
+            }
 
             DataTable result = ExecuteQuery(msg);
 
@@ -110,13 +111,13 @@ public static class DBController
                 string status = row["status"].ToString();
                 string role = row["role"].ToString();
 
-                _id.Text += clientId + "\n";
-                _firstname.Text += firstName + "\n";
-                _lastname.Text += lastName + "\n";
-                _balance.Text += balance + "\n";
-                _username.Text += username + "\n";
-                _status.Text += status + "\n";
-                _role.Text += role + "\n";
+                labels[0].Text += clientId + "\n";
+                labels[1].Text += firstName + "\n";
+                labels[2].Text += lastName + "\n";
+                labels[3].Text += balance + "\n";
+                labels[4].Text += username + "\n";
+                labels[5].Text += status + "\n";
+                labels[6].Text += role + "\n";
             }
         }
         catch (Exception e)
